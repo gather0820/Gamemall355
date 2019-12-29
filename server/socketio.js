d9cefe999d6cb1ccd1b5f9bf29cd44d7aa95270a
gather0820 <54766174+gather0820@users.noreply.github.com>
2019-12-29 19:42:25 +0000

    Update socketio.js

--- server/socketio.js
+++ server/socketio.js
@@ -1,7 +1,7 @@
 var socket_io = require('socket.io')
-var socketio = {} // 获取io
-let clientList = {} // 客户端连接用户
-let adminList = {} // 客户端连接用户
+var socketio = {} // 获取io Get io
+let clientList = {} // 客户端连接用户 The client connects to the user
+let adminList = {} 
 let io = null
 
 socketio.getSocketio = function(server) {
@@ -17,11 +17,7 @@ socketio.getSocketio = function(server) {
       token
     }
     console.log(adminList)
-    // socket.on('setToken', (res)=>{
-    // }
-    // 离线
     socket.on('disconnect', function(item) {
-      // 这里监听 disconnect，就可以知道谁断开连接了
       console.log('disconnect: ' + socket.id)
       delete adminList[socket.id]
       console.log(adminList)
@@ -37,10 +33,7 @@ socketio.getSocketio = function(server) {
       token
     }
     console.log(clientList)
-    // socket.emit('noticeInfo', '这还是一个通知')
-    // 离线
     socket.on('disconnect', function(item) {
-      // 这里监听 disconnect，就可以知道谁断开连接了
       console.log('disconnect: ' + socket.id)
       delete clientList[socket.id]
     })
